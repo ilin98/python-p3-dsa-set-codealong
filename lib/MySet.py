c16de0f91fcee0d1d2bdc5864d90875fb95a2453
Isaac <isaac.lin.pnc@gmail.com>
2023-04-18 10:19:11 -0400

    finished lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,26 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def delete(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictoinary.clear()
+
+    def __str__(self):
+        print(self.dictionary)
